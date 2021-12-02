b4df2457d58156e8b71914116a7f8de60270c2a8
Zhi Yong Han <hanzyong0@gmail.com>
2021-12-02 16:52:52 +0800

    add search feature

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
+const span = document.querySelector('.error');
 const submit = document.querySelector('#submit');
-const h1 = document.querySelector('h1');
 
 async function getWeather(text) {
   try {
     const key = `http://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&APPID=2c7dbd7f96c6c397de8f494d78aef1b4`;
     const response = await fetch(key, { mode: 'cors' });
     const data = await response.json();
-    console.log({ temperature: data.main.temp });
-    h1.textContent = `${text}\'s temperature is ${data.main.temp} degrees celsius`;
+    span.style.display = 'none';
+    console.log(data);
+    setTitle(data.name);
+    setDesc(data.weather[0].description);
+    setTemp(data.main.temp);
+    setFeelsLike(data.main.feels_like);
+    setWind(data.wind.speed);
+    setHumidity(data.main.humidity);
   } catch (err) {
-    h1.textContent = 'Data not available';
+    span.style.display = 'inline';
+    span.textContent = "Couldn't find location"
   }
 };
 
+// Search button
 submit.addEventListener('click', () => {
   const location = document.querySelector('#location');
   getWeather(location.value);
 });
+
+window.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    submit.click();
+    e.preventDefault();
+  };
+});
+
+function setTitle(text) {
+  const title = document.querySelector('.title');
+  title.textContent = text;
+}
+
+function setDesc(text) {
+  const desc = document.querySelector('.description');
+  desc.textContent = text;
+}
+
+function setTemp(text) {
+  const temp = document.querySelector('.temperature');
+  temp.textContent = Math.round(text * 10) / 10;
+}
+
+function setFeelsLike(text) {
+  const feelsLike = document.querySelector('.feels-like');
+  feelsLike.textContent = `Feels like: ${text} ℃`
+}
+
+function setWind(text) {
+  const wind = document.querySelector('.wind');
+  wind.textContent = `Wind: ${text} meter/sec`;
+}
+
+function setHumidity(text) {
+  const humidity = document.querySelector('.humidity');
+  humidity.textContent = `Humidity: ${text}%`;
+}
+
+// Default city
+getWeather('kuala lumpur');
